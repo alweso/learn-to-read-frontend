b4f49278d2e41de6095fb86aab20cba4aa5d64c7
Aleksandra <alweso@gmail.com>
2024-08-02 14:41:54 +0200

    added sound on click and closing icon

--- src/App.tsx
+++ src/App.tsx
@@ -26,3 +26,7 @@ function App() {
 }
 
 export default App
+
+
+// credits:
+// <a href="https://www.flaticon.com/free-icons/volume" title="volume icons">Volume icons created by Bharat Icons - Flaticon</a>
--- src/components/SingleCard/SingleCard.tsx
+++ src/components/SingleCard/SingleCard.tsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import wordsList from '../../constants/wordlist'
 import { useLoading } from '../../hooks/useLoading'
 import Loader from '../Loader/Loader'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVolumeUp, faClose } from '@fortawesome/free-solid-svg-icons';
 
 const SingleCard = () => {
   const params = useParams()
@@ -38,12 +40,12 @@ const SingleCard = () => {
     checkAllImagesLoaded([selectedCard?.imageBig || '']) // Pass an array with the image URLs to check
   }, [params.card, checkAllImagesLoaded])
 
-  useEffect(() => {
-    // Check if cardData is defined before playing sound
-    if (cardData) {
-      playSound()
-    }
-  }, [cardData])
+  // useEffect(() => {
+  //   // Check if cardData is defined before playing sound
+  //   if (cardData) {
+  //     playSound()
+  //   }
+  // }, [cardData])
 
   if (loading || !cardData) {
     // Display a loading indicator or error message here
@@ -51,16 +53,24 @@ const SingleCard = () => {
   }
 
   return (
-    <div className="flex justify-center pt-20">
-      <div className="card w-[600px] bg-white rounded-md p-2 shadow-lg cursor-pointer border border-gray-300">
-        <img
+    <div className="flex justify-center pt-20 " >
+      <div onClick={playSound} className="card w-[600px] relative bg-white rounded-md p-2 shadow-lg cursor-pointer border border-gray-300">
+      <div className="absolute right-5 top-5">
+            <FontAwesomeIcon icon={faClose} size='2x' />
+          </div>
+      <img
           src={cardData.imageBig}
           alt={cardData.word}
           className="w-full rounded-md"
         />
-        <h1 className="uppercase text-6xl font-sans font-bold text-gray-800 flex justify-center py-2">
-          {cardData.word}
-        </h1>
+        <div className="relative flex items-center py-2">
+          <h1 className="uppercase text-6xl font-sans font-bold text-gray-800 flex-1 text-center">
+            {cardData.word}
+          </h1>
+          <div className="absolute right-5">
+            <FontAwesomeIcon icon={faVolumeUp} size='2x' />
+          </div>
+        </div>
       </div>
     </div>
   )
